d5d5335aa87c0ccd2d70d0fbc6ac8b169e41ce2c
andreasvonburg <a@von-burg.net>
2021-02-18 21:22:35 +0100

    hook and subsystem prop

--- src/app/page/rocket/ei/field/PageHookEiProp.php
+++ src/app/page/rocket/ei/field/PageHookEiProp.php
@@ -1,13 +1,8 @@
 <?php
 namespace page\rocket\ei\field;
 
-use rocket\ei\util\Eiu;
-use n2n\util\type\CastUtils;
-use n2n\impl\web\dispatch\mag\model\EnumMag;
-use n2n\web\dispatch\mag\Mag;
-use rocket\ei\component\prop\indepenent\EiPropConfigurator;
+use page\rocket\ei\field\conf\PageHookKeyConfig;
 use rocket\impl\ei\component\prop\enum\EnumEiProp;
-use page\rocket\ei\field\conf\PageHookEiPropConfigurator;
 
 class PageHookEiProp extends EnumEiProp {
 
@@ -15,18 +10,19 @@ class PageHookEiProp extends EnumEiProp {
 // 		return new PageHookEiPropConfigurator($this);
 // 	}
 	
-	public function isMandatory(Eiu $eiu): bool {
-		return false;
+	function prepare() {
+		$this->getConfigurator()->removeAdaption($this->getEnumConfig());
+		$this->getConfigurator()->addAdaption(new PageHookKeyConfig($this->getEnumConfig()));
 	}
 	
-	public function createMag(Eiu $eiu): Mag {
-		$mag = parent::createMag($eiu);
-		CastUtils::assertTrue($mag instanceof EnumMag);
+// 	public function createMag(Eiu $eiu): Mag {
+// 		$mag = parent::createMag($eiu);
+// 		CastUtils::assertTrue($mag instanceof EnumMag);
 		
-		if (null !== ($characteristicsKey = $eiu->entry()->getValue($this))){
-			$mag->setOptions(array_merge(array($characteristicsKey => $characteristicsKey), $mag->getOptions()));
-		}
+// 		if (null !== ($characteristicsKey = $eiu->entry()->getValue($this))){
+// 			$mag->setOptions(array_merge(array($characteristicsKey => $characteristicsKey), $mag->getOptions()));
+// 		}
 		
-		return $mag;
-	}
+// 		return $mag;
+// 	}
 }
