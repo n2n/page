80280cc1a155418e0470b40ad960412b54f685bf
andy <a@von-burg.net>
2022-07-11 13:05:42 +0200

    EntityListeners must be Lookupable in n2n 7.3

--- src/app/page/bo/PageEntityListener.php
+++ src/app/page/bo/PageEntityListener.php
@@ -4,8 +4,9 @@ namespace page\bo;
 use n2n\reflection\ObjectAdapter;
 use page\model\PageMonitor;
 use n2n\context\ThreadScoped;
+use n2n\context\RequestScoped;
 
-class PageEntityListener extends ObjectAdapter implements ThreadScoped {
+class PageEntityListener extends ObjectAdapter implements RequestScoped {
 	
 	public function _postInsert(PageMonitor $pageMonitor) {
 		$pageMonitor->flush();
