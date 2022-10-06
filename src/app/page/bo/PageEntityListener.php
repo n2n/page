bcc25fae30f8dd4ec65ac6c14f155ca63f789410
Andreas von Burg <a@von-burg.net>
2022-10-06 19:16:47 +0200

    n2n 7.3 compatibility

--- src/app/page/bo/PageEntityListener.php
+++ src/app/page/bo/PageEntityListener.php
@@ -3,17 +3,18 @@ namespace page\bo;
 
 use n2n\reflection\ObjectAdapter;
 use page\model\PageMonitor;
+use n2n\context\RequestScoped;
 
-class PageEntityListener extends ObjectAdapter {
-	
+class PageEntityListener extends ObjectAdapter implements RequestScoped {
+
 	public function _postInsert(PageMonitor $pageMonitor) {
 		$pageMonitor->flush();
 	}
-	
+
 	public function _postUpdate(PageMonitor $pageMonitor) {
 		$pageMonitor->flush();
 	}
-	
+
 	public function _postRemove(PageMonitor $pageMonitor) {
 		$pageMonitor->flush();
 	}
